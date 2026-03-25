872174e056b53b029ec5cff5406ee3f278f45e6b
Hippolyte <hippolyte.devweb@gmail.com>
2026-03-25 10:25:55 +0100

    fix (schema) : fix every prisma issue for schema validation

--- prisma.config.ts
+++ prisma.config.ts
@@ -6,10 +6,10 @@ import { defineConfig, env } from "prisma/config"
 
 export default defineConfig({
     schema: "prisma/schema.prisma",
-    migration : {
+    migrations : {
         path:  "prisma/migrations"
     },
-    datasource: env("DIRECT_URL") ?? env("DATABASE_URL"),
+    datasource: { url : env("DIRECT_URL") ?? env("DATABASE_URL")},
 
     // TABLES EXTERNE DE BETTER AUTH, PRISMA A BESOIN DE SAVOIR //
     experimental: {externalTables: true},
