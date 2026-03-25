0bca1240575d51a16a335dad2b2c47e5e5ef6819
Hippolyte <hippolyte.devweb@gmail.com>
2026-03-25 09:12:40 +0100

    feat (prisma/config) : create config prisma file for CLI

---
+++ prisma.config.ts
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config"
+
+
+// CONFIG POUR LE CLI DE PRISMA //
+
+export default defineConfig({
+    schema: "prisma/schema.prisma",
+    migration : {
+        path:  "prisma/migrations"
+    },
+    datasource: env("DIRECT_URL") ?? env("DATABASE_URL"),
+
+    // TABLES EXTERNE DE BETTER AUTH, PRISMA A BESOIN DE SAVOIR //
+    experimental: {externalTables: true},
+    tables: {
+        external: ["auth.user", "auth.session", "auth.account", "auth.verification"]
+    
+    }
+})
