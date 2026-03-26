3003ae7ae831f9f987c2d96379cd4f0d3dceeef1
Hippolyte <hippolyte.devweb@gmail.com>
2026-03-26 11:56:34 +0100

    fix (auth): add return to auth Session

--- lib/authSession.ts
+++ lib/authSession.ts
@@ -2,7 +2,7 @@ import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 // Fonction pour récupéré la session en SSR //
 export const authSession = async () => {
-  await auth.api.getSession({
+  return await auth.api.getSession({
     headers: await headers(),
   });
 };
