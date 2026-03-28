173210bf8a4fda1055205342b8a2f8575507814a
Hippolyte <hippolyte.devweb@gmail.com>
2026-03-28 13:32:22 +0100

    feat(auth): handle user creation on userProfile and StaffProfile logic

--- lib/auth.ts
+++ lib/auth.ts
@@ -21,7 +21,84 @@ const createAuth = () =>
         clientSecret: process.env.MICROSOFT_CLIENT_SECRET!,
         tenantId: process.env.MICROSOFT_TENANT_ID!,
       },
+      // ICI ONT RAJOUEZ UN CHAMPS A USER DANS BETTER AUTH POUR SAVOIR SI USER DEVRAS ETRE CREER DANS BACKOFFICE OU PUBLIC //
     },
+    user: {
+      additionalFields: {
+        accountType: {
+          type: ["public", "backoffice"],
+          required: true,
+          input: false,
+        },
+        trackingData: {
+          type: "json",
+          required: false,
+          input: false,
+        },
+      },
+    },
+    // Hooks avant qu'un user sois créer, User sera placé sois dans StaffProfil
+    // sois dans User Profile selon le account type affecté lors de le création
+    //  de son compte. //
+    databaseHooks: {
+      user: {
+        create: {
+          before: async (user) => {
+            // TS N'arrive pas a reconnaitres les extra fields dans ce Hooks, ont doit donc l'aidez//
+
+            // Ont type les datas Tracké //
+            type TrackingData = {
+              utm_source?: string | null;
+              utm_medium?: string | null;
+              utm_campaign?: string | null;
+              referrer_domain?: string | null;
+              language?: string | undefined;
+              hasAcceptedCookies?: boolean;
+              visitCount?: number | null;
+              createdAt?: string | null;
+            };
+
+            // ICI Ont type les fields supplémentaires //
+            type userExtraFieldsTyped = {
+              accountType: "public" | "backoffice";
+              trackingData?: TrackingData | null;
+            };
+
+            // ET la ont fusionne le type de User avec les types des fields supplémentaires //
+            const typedUser = user as typeof user & userExtraFieldsTyped;
+            // typedUser est donc User mais avec tout les champs bien typé car TS ne pouvais pas le faire //
+
+            if (typedUser.accountType === "public") {
+              await myPrisma.userProfile.create({
+                data: {
+                  displayName: user.name,
+                  avatarUrl: user.image,
+                  hasAcceptedCookies:
+                    typedUser?.trackingData?.hasAcceptedCookies,
+                  visitCountBeforeLogin: typedUser?.trackingData?.visitCount,
+                  language: typedUser?.trackingData?.language ?? "unknonw",
+                  utm_source: typedUser?.trackingData?.utm_source,
+                  utm_campaign: typedUser?.trackingData?.utm_medium,
+                  utm_medium: typedUser?.trackingData?.utm_campaign,
+                  referrer_domain: typedUser?.trackingData?.referrer_domain,
+                  anonymeCreatedAt: typedUser?.trackingData?.createdAt,
+                },
+              });
+            }
+            if (typedUser.accountType === "backoffice") {
+              await myPrisma.staffProfile.create({
+                data: { displayName: user.name },
+              });
+            }
+
+            return {
+              data: { ...user, accountType: typedUser },
+            };
+          },
+        },
+      },
+    },
+
     plugins: [nextCookies()],
   });
 
