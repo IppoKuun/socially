4345f813a8be4351451343ba2a402df294ec4379
Hippolyte <hippolyte.devweb@gmail.com>
2026-04-04 16:37:58 +0200

    feat (onboading) add framer motion animation

--- app/[locale]/onboarding/page.tsx
+++ app/[locale]/onboarding/page.tsx
@@ -4,32 +4,43 @@ import { myPrisma } from "@/lib/prisma";
 import StepFinal from "./_components/StepFinal";
 import StepTwo from "./_components/StepTwo";
 import StepOne from "./_components/StepOne";
+import { StepAnimator } from "./_components/StepAnimator";
 
 export default async function OnboardingPage() {
   // Si user a déjà onboarded on l'envoie a feed //
   const session = await getSession();
-  if (session) {
-    const id = session.user.id;
-    const user = await myPrisma.userProfile.findUnique({
-      where: { userId: id },
-    });
-
-    if (user?.hasOnboarded) {
-      redirect("/feed");
-    }
-    const onboardingStep = user?.onboardedStep;
-    const renderStep = () => {
-      switch (onboardingStep) {
-        case 0:
-          return (
-            <StepOne user={session.user} providerImage={session?.user?.image} />
-          );
-        case 1:
-          return <StepTwo />;
-        case 2:
-          return <StepFinal />;
-      }
-    };
+  if (!session) {
+    redirect("/login");
   }
-  return null;
+
+  const id = session.user.id;
+  const user = await myPrisma.userProfile.findUnique({
+    where: { userId: id },
+  });
+
+  if (user?.hasOnboarded) {
+    redirect("/feed");
+  }
+
+  const onboardingStep = user?.onboardedStep;
+
+  const renderStep = () => {
+    switch (onboardingStep) {
+      case 0:
+        return (
+          <StepOne user={session.user} providerImage={session?.user?.image} />
+        );
+      case 1:
+        return <StepTwo />;
+      case 2:
+        return <StepFinal />;
+    }
+  };
+
+  return (
+    <section className="w-full">
+      {/* On passe le step comme clé à notre animateur */}
+      <StepAnimator step={onboardingStep}>{renderStep()}</StepAnimator>
+    </section>
+  );
 }
