a8155ac4c00c40d8a248e8f9f164f1c9da2d41c2
Hippolyte <hippolyte.devweb@gmail.com>
2026-04-12 11:19:32 +0200

    feat(zod): add schema to image and post model

---
+++ lib/validations.ts/post.ts
@@ -0,0 +1,45 @@
+import { z } from "zod";
+import { Category, ModerationStatus } from "@prisma/client";
+
+const postLimitSize = 8000000;
+const ACCEPTED_IMAGE_TYPES = [
+  "image/jpeg",
+  "image/jpg",
+  "image/png",
+  "image/webp",
+];
+
+export const uploadImageSchema = z.object({
+  image: z
+    .any() // On utilise any car 'File' n'existe pas côté serveur en standard TS pur
+
+    // On vérifie que le type et taille sois cohérent //
+    .refine((file) => file?.size <= postLimitSize, {
+      params: {
+        // Obligé de définir les clé pour i18n car c'est une erreur personnalisé et zod n'as pas de code pourça //
+        i18n: {
+          key: "validation.imageTooLarge",
+          values: {
+            maximum: postLimitSize / 1000000,
+          },
+        },
+      },
+    })
+    .refine((file) => ACCEPTED_IMAGE_TYPES.includes(file?.type), {
+      params: {
+        i18n: "validation.invalidImageType",
+      },
+    }),
+});
+
+export const postSchema = z.object({
+  title: z.string().max(50).trim().min(3),
+  content: z
+    .string()
+    .max(500)
+    .optional()
+    .transform((v) => (v === "" ? null : v)),
+  imagesUrl: z.url().max(5).optional(),
+});
+
+export type postSchemaType = z.infer<typeof postSchema>;
