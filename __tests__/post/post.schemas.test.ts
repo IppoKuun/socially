ad8e5eaa723253d8bbb87a19dbb9a434ff03777a
Hippolyte <hippolyte.devweb@gmail.com>
2026-04-12 11:29:40 +0200

    add basic test to post zod post schema

---
+++ __tests__/post/post.schemas.test.ts
@@ -0,0 +1,36 @@
+import { postSchema } from "@/lib/validations.ts/post";
+import { title } from "process";
+
+describe("post schema", () => {
+  it("normalise and accept a valid input", () => {
+    const result = postSchema.safeParse({
+      title: "    Les meuilleurs IA pour dev   ",
+      content: "Codex et Claude",
+      imagesUrl: "https://chatgpt.com/fr-FR",
+    });
+
+    expect(result.success).toBe(true);
+    expect(result.data?.title).toBe("Les meuilleurs IA pour dev");
+    expect(result.data?.imagesUrl).toBe("https://chatgpt.com/fr-FR");
+  });
+  it("reject if title is not present", () => {
+    const result = postSchema.safeParse({
+      content: "ChatGPT est la meuilleur IA",
+      imagesUrl: "https://chatgpt.com/fr-FR",
+    });
+
+    expect(result.success).toBe(false);
+    expect(result.error?.flatten().fieldErrors.title).toBeDefined();
+  });
+
+  it("reject a bad images URL", () => {
+    const result = postSchema.safeParse({
+      title: "Nouveau modele gemini",
+      content: "Le nouveau modele Gemin est meuilleur que tout les autres",
+      imagesUrl: "gemini.fr",
+    });
+
+    expect(result.success).toBe(false);
+    expect(result.error?.flatten().fieldErrors.imagesUrl).toBeDefined();
+  });
+});
