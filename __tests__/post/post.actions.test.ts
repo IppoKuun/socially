550084f49a36406e0973ddf44eb63705ed1057db
Hippolyte <hippolyte.devweb@gmail.com>
2026-04-13 16:09:09 +0200

    feat (post test): add mock fonction for jest test

--- __tests__/post/post.actions.test.ts
+++ __tests__/post/post.actions.test.ts
@@ -7,5 +7,43 @@
 // Title/Content/Image : ZOD ok, cloudindary fail, erreur appropriée*
 // Title/Content/image : ZOD accept, IA UNCERTAINS sur texte, slug deja pris, reffait un nv slug, return ok avec moderations unsafe
 // Title/Content/Image : Tout doit bien passé et return ok: true, userMsg:"" //
+// Tout est bon mais rateLimits atteints //
 
 const mockModerationPost = jest.fn();
+const mockGetSession = jest.fn();
+const mockUserProfileFindUnique = jest.fn();
+const mockRateLimit = jest.fn();
+const mockuploadCloudinary = jest.fn();
+const mockCreatePost = jest.fn();
+
+jest.mock("@/lib/cloudinaryCondfig", () => ({
+  uploadCloudinary: (...args: unknown[]) => mockuploadCloudinary(...args),
+}));
+
+jest.mock("@/lib/authSession", () => ({
+  getSession: (...args: unknown[]) => mockGetSession(...args),
+}));
+
+jest.mock("@/lib/IA", () => ({
+  moderatePostContent: (...args: unknown[]) => mockModerationPost(...args),
+}));
+
+jest.mock("@/lib/prisma", () => ({
+  myPrisma: {
+    userProfile: {
+      findUnique: (...args: unknown[]) => mockUserProfileFindUnique(...args),
+    },
+  },
+}));
+
+jest.mock("@/lib/prisma", () => ({
+  myPrisma: {
+    Post: {
+      create: (...args: unknown[]) => mockCreatePost(...args),
+    },
+  },
+}));
+
+jest.mock("@/lib/rateLimits", () => ({
+  rateLimits: (...args: unknown[]) => mockRateLimit(...args),
+}));
