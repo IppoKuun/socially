5b27b70bbf78c484828f8de3d2abc118767b2b98
Hippolyte <hippolyte.devweb@gmail.com>
2026-04-13 11:59:16 +0200

    refactor (post creation action) : optimize function order and somme minor issur=e on serv action + comment

--- app/actions/post.ts
+++ app/actions/post.ts
@@ -14,29 +14,44 @@ export default async function createPost(
   _prevstate: FormState,
   FormData: FormData,
 ) {
+  const session = await getSession();
+  if (!session) {
+    return { ok: false, userMsg: " Veuillez vous connctez pour publiez" };
+  }
+  const user = await myPrisma.userProfile.findUnique({
+    where: { userId: session.user.id },
+  });
   const errorMap = await getZodErrorMapForRequest();
 
   const raw = Object.fromEntries(FormData);
 
-  const images = FormData.getAll("file").filter(
+  // On vérifie que chaque images a le bon type, sinon erreur //
+  const rawFiles = FormData.getAll("images").every(
     (entry): entry is File => entry instanceof File,
   );
 
+  if (!rawFiles) {
+    return { ok: false, userMsg: "Veuillez entrez que des images" };
+  }
+
+  // Parsing de chaque image avec Zod, qui vas nous permettre d'upload sur Cloudinary
+  // Et enfin, envoyez les images a l'IA moderation qui a besoin de ces URL pour modéré les images//
+
+  const images = FormData.getAll("images");
   const imagesParsed = images.map((image) => {
-    const result = uploadImageSchema.safeParse(image);
+    const result = uploadImageSchema.safeParse(image, { error: errorMap });
 
     if (!result.success) {
       return {
         ok: false,
+        userMsg: "Erreur lors de l'envoie de votre image",
         errors: result.error.flatten().fieldErrors,
       };
     }
   });
 
-  if (!imagesParsed) {
-  }
   const results = await Promise.all(
-    images.map((image) => uploadCloudinary(image)),
+    images.map((image) => uploadCloudinary(image as File)),
   );
   if (!results) {
     return {
@@ -68,10 +83,17 @@ export default async function createPost(
         cloudinary.uploader.destroy(id, { resource_type: "image" }),
       ),
     );
-
+    // Ici, ont logge l'erreur pour nous meme et pas pour User car aucune utilisé a ce qu'il sois
+    // au courant que l'image n'est pas réussi a cloudinary Après que son poste est été jugée unsafe //
     if (!result) {
       console.error("Impossible de supprimé images après rejets d'IA");
     }
+
+    return {
+      ok: false,
+      userMsg: "Votre contenue viole notre politique d'utilisation",
+      reasons: moderation.reasons,
+    };
   }
 
   const parsed = postSchema.safeParse({ raw }, { error: errorMap });
@@ -90,6 +112,7 @@ export default async function createPost(
     const verifySlug = await myPrisma.post.findUnique({
       where: { slug: slug },
     });
+    // Tant qu'on trouve un slug existant, on refait la fonction //
     while (verifySlug) {
       return await generateSlug();
     }
@@ -98,17 +121,9 @@ export default async function createPost(
 
   const postSlug = await generateSlug();
 
-  const session = await getSession();
-  if (!session) {
-    return { ok: false, userMsg: " Veuillez vous connctez pour publiez" };
-  }
-  const user = await myPrisma.userProfile.findUnique({
-    where: { id: session.user.id },
-  });
-
   const created = await myPrisma.post.create({
     data: {
-      title: parsed.data!.title,
+      title: parsed.data.title,
       moderationStatus: moderation.moderationStatus,
       slug: postSlug,
       content: parsed.data?.content,
