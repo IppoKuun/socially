41116ce95a2978f4b1d88f42069d86749c4741fa
Hippolyte <hippolyte.devweb@gmail.com>
2026-05-06 18:56:30 +0200

    feat (settingsBlocks) :  add block queries with cursor

---
+++ lib/settings/block/queries.ts
@@ -0,0 +1,66 @@
+import { getSession } from "@/lib/authSession";
+import { myPrisma } from "@/lib/prisma";
+
+type blockItem = {
+  avatarUrl: string | null;
+  username: string | null;
+  id: string;
+};
+
+type getUserBlockListType = {
+  nextCursor: string | undefined;
+  blocksProfilToDisplay: blockItem[];
+};
+
+export async function getUserBlockList(
+  cursor?: string,
+): Promise<getUserBlockListType> {
+  const session = await getSession();
+
+  if (!session) {
+    throw new Error("Unauthorized");
+  }
+
+  const profile = await myPrisma.userProfile.findUnique({
+    where: { userId: session.user.id },
+    select: { id: true },
+  });
+  if (!profile) {
+    throw new Error("Profile not found");
+  }
+
+  // Les profiles delete seront aussi affiché dans la liste de block
+  // C'est un choix métier //
+  const blockList = await myPrisma.block.findMany({
+    take: 21,
+    ...(cursor && {
+      cursor: { id: cursor },
+      skip: 1,
+    }),
+    orderBy: { createdAt: "desc" },
+    select: {
+      id: true,
+      blocker: { select: { id: true, avatarUrl: true, username: true } },
+    },
+  });
+
+  const hasNextPage = blockList.length > 20;
+  // Si la longueur de la blockLIST est supérieux a 20 cv dire qu'il ya une page suivante
+  // Ducoup ont la prends en coupant l'élement suivant qui est le prochain cursor
+  // Sinon on prends le block telle qu'il l'est//
+  const rawBlocks = hasNextPage ? blockList.slice(0, 20) : blockList;
+
+  const nextCursor = blockList[20]?.id;
+  const blocksProfilToDisplay: blockItem[] = rawBlocks.map((item) => {
+    return {
+      id: item.blocker.id,
+      username: item.blocker.username,
+      avatarUrl: item.blocker.avatarUrl,
+    };
+  });
+
+  return {
+    nextCursor,
+    blocksProfilToDisplay,
+  };
+}
